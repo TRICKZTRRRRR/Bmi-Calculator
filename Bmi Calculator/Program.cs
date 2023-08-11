a9f917a19c486ba77ba784d76b9b66d1bed5afea
TRICKZTRRRRR <jonas04@outlook.dk>
2023-08-11 12:15:59 +0200

    Lavede Bmi Udregner

--- Bmi Calculator/Program.cs
+++ Bmi Calculator/Program.cs
@@ -10,6 +10,47 @@ namespace Bmi_Calculator
     {
         static void Main(string[] args)
         {
+            Console.Title = "BMI Calculator";
+
+            while (true)
+            {
+
+                Console.Clear();
+                Console.WriteLine("Weigth");
+                string cmWeight = Console.ReadLine();
+                Console.WriteLine("\r\nHeight");
+                string cmHeight = Console.ReadLine();
+
+                double cmW, cmH;
+                double.TryParse(cmWeight, out cmW);
+                double.TryParse(cmHeight, out cmH);
+
+                double cmCalculation = (cmW / Math.Pow((cmH), 2)) * 10000;
+
+                Console.WriteLine("\r\nBMI = " + cmCalculation.ToString("0.##"));
+
+                if (cmCalculation < 18.5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("You are underweight");
+                }
+                else if (cmCalculation >= 18.5 && cmCalculation < 25)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("You are Normal weight");
+                }
+                else if (cmCalculation >= 25 && cmCalculation < 30)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are Obese");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("You are too Fat");
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
