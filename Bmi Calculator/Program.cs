ea537b0cb7e7116b6cac446658301da47ace6805
TRICKZTRRRRR <jonas04@outlook.dk>
2023-08-11 12:34:22 +0200

    Ændrede Program.cs

--- Bmi Calculator/Program.cs
+++ Bmi Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,45 +11,72 @@ namespace Bmi_Calculator
     {
         static void Main(string[] args)
         {
+            // Set the title of the console window
             Console.Title = "BMI Calculator";
 
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Start an infinite loop for continuous calculations
             while (true)
             {
-
+                // Clear the console screen
                 Console.Clear();
-                Console.WriteLine("Weigth");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                // Prompt for weight input
+                Console.WriteLine("Weight");
                 string cmWeight = Console.ReadLine();
+
+                // Prompt for height input
                 Console.WriteLine("\r\nHeight");
                 string cmHeight = Console.ReadLine();
 
                 double cmW, cmH;
+
+                // Convert input strings to numeric values
                 double.TryParse(cmWeight, out cmW);
                 double.TryParse(cmHeight, out cmH);
 
+                // Calculate BMI using the formula: BMI = (weight / height^2) * 10000
                 double cmCalculation = (cmW / Math.Pow((cmH), 2)) * 10000;
 
+                // Display the calculated BMI
                 Console.WriteLine("\r\nBMI = " + cmCalculation.ToString("0.##"));
 
+                // Determine BMI category and display appropriate message with color
                 if (cmCalculation < 18.5)
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("You are underweight");
                 }
-                else if (cmCalculation >= 18.5 && cmCalculation < 25)
+                else if (cmCalculation >= 18.5 && cmCalculation < 24)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("You are Normal weight");
                 }
-                else if (cmCalculation >= 25 && cmCalculation < 30)
+                else if (cmCalculation >= 24 && cmCalculation < 30)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("You are Obese");
+                }
+                else if (cmCalculation >= 30 && cmCalculation < 35)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("You are Obese First Degree");
+                }
+                else if (cmCalculation >= 35 && cmCalculation < 40)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("You are Obese");
+                    Console.WriteLine("You are Obese Second Degree");
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("You are too Fat");
+                    Console.WriteLine("You are Obese Third Degree");
                 }
+
+                // Wait for user to press Enter before restarting the loop
                 Console.ReadLine();
             }
         }
